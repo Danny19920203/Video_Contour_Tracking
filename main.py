1249b0760dbeccd0b4ebaf80d56342e642e14798
zweng4 <w2280518650@gmail.com>
2018-03-08 10:38:19 -0800

    use background subtraction

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from load_frameInfo import loadInfo
 # Initalize camera
 cap = cv2.VideoCapture("test.mp4")
-
+fgbg = cv2.BackgroundSubtractorMOG()
 # define range of purple color in HSV
 lower_purple = np.array([165,50,90])
 upper_purple = np.array([179,255,255])
@@ -19,25 +19,26 @@ allFrames = loadInfo("test.mp4", option=2)
 while True:
     if cap.grab():
         flag, frame = cap.retrieve()
-        boundingBoxes = allFrames[frame_count]
+        #boundingBoxes = allFrames[frame_count]
         hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         print (np.shape(hsv_img))
         # Threshold the HSV image to get only blue colors
-        mask = cv2.inRange(hsv_img, lower_purple, upper_purple)
-        mask[0:Height, 0:Width] = 0
+        mask = fgbg.apply(frame)
         print (np.shape(mask))
-        for i in range(len(boundingBoxes)):
-            boundingBox = boundingBoxes[i]
-            x = boundingBox[0]
-            y = boundingBox[1]
-            w = boundingBox[2]
-            h = boundingBox[3]
-            mask[y:y + h, x:x + w] = 1
+        # mask[0:Height, 0:Width] = 0
+        # print (np.shape(mask))
+        # for i in range(len(boundingBoxes)):
+        #     boundingBox = boundingBoxes[i]
+        #     x = boundingBox[0]
+        #     y = boundingBox[1]
+        #     w = boundingBox[2]
+        #     h = boundingBox[3]
+        #     mask[y:y + h, x:x + w] = 1
 
         #print (mask[0:10, 30:40])
         # Find contours, OpenCV 3.X users use this line instead
-        _, contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #_, contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         if len(contours) > 0:
             cv2.drawContours(frame, contours, -1, (0,255,0), 3)
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+import numpy as np
 import cv2
-cap = cv2.VideoCapture("test.mp4")
-
-while True:
-    if cap.grab():
-        flag, frame = cap.retrieve()
-        if not flag:
-            continue
-        else:
-            cv2.imshow('video', frame)
-    if cv2.waitKey(10) == 27:
+cap = cv2.VideoCapture('test.mp4')
+fgbg = cv2.BackgroundSubtractorMOG()
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    cv2.imshow('frame',fgmask)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
+cap.release()
+cv2.destroyAllWindows()
